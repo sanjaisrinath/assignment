db68d653f41ecb9bf727f81fa171305fd0518b83
sanjaisrinath <134049949+sanjaisrinath@users.noreply.github.com>
2025-04-18 02:54:22 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,379 @@
+# '''variables stores the memory allocation of the value
+# a = 11
+# print(a)
+# _ = 22
+# print(_)
+
+# operators:
+# arthiemetic Operators (+,-,/,*)
+# comparison or rational operators (==,=<,=>,!=,<,>)
+# logical operators (and,or,not)
+# assignment operators (==,=+,=-,=\,=\\,=**,%=,=*)
+# membership (in,not in)
+# bitwise(AND(&),OR|,XOR^,NOT~,RIGHT SHIFT>>,LEFT WISE<<)
+# identity(is,is not)
+
+# datatypes:
+# numbers()
+# int,float
+# a=2344(int) a= 12.33(float)
+# list()
+# sequence of anything inside [] seprated with comma,
+# a = ["ass","ddrer","sdf"]
+# string()
+# sequence of anything decleared inside ""
+# name="sanjai" location="india"
+# tuples()
+# sequence of anything decleared inside () seprated with comma
+# a=("dff",'fejjfee',"fjefjef")
+# dictonary()
+# sequence of key:value inside {} seprated with comma
+# a={'name':'sanjai','age':34,'email':'sanjaisan001@gmail.com'} 
+
+# a = 1
+# b = 20300
+# print(a+b)
+# print(a-b)
+# print(a/b)
+# print(a*b)
+# print(a%b)
+# print(a//b)
+
+# print(1+True)
+# print(1+False)
+
+# a  b       a and b     a or b      not a           true = 1  false = 0
+# t  t          t          t          f 
+# t  f          f          t          f  
+# f  t          f          t          t 
+# f  f          f          f          f
+
+
+# a = 20
+# b = 10
+
+# print(a<10 or b > 20)
+# print(a*b)
+# print(a==20 and b==1)
+
+# name= 'sanjai'
+# email= 'sanjai@gmail.com'
+# password= 'sss'
+# print(name=='sanjai' and  email=='sanjai@gmail.com' and password=='sss')
+# print(type(a)) 
+
+# name = (input("Enter the name: "))
+# print(name)
+# print(f"your name is {name}\n")
+# print(type(name))
+
+# firstname= "sanjai"
+# surname="srinath"
+# print (4*(f" {firstname}{surname} \n"))
+
+# cardno = 1234
+# print(f"xxxxxx{cardno}")
+
+
+
+# slicing
+# ss= "sanjaisrinath"
+# print(ss [:4] + ss[8:])
+
+# boolean
+# f=True
+# R=False
+# print(f<R)'''
+
+# # x= 23
+# # x +=1
+# # print(x)
+
+# # sum = 12
+# # sum = 23
+# # print(sum+sum)
+
+# # sum1 = 52
+# # sum2 = 36
+# # print(sum1-sum2)
+
+# # sum3 = 23
+# # sum4 = 56
+# # print(sum3*sum4)
+
+# # sum5 = 36
+# # print(sum5/5)
+
+# # sum6 = 67
+# # print(sum6/4)
+
+# # sum7 = 48
+# # print(sum7%7)
+
+# # sum8 = 2
+# # print(2**8)
+
+# # sum9 = "edupoly"
+# # sum10 = "Traning"
+# # print(sum9,sum10)
+
+
+# # perimeter = 4
+# # x = perimeter*5
+# # print(f"perimeter of square is:{x} cm")
+
+
+# # x_lenght = 50
+# # y_breadth = 35
+# # x = 2*(x_lenght + y_breadth)
+# # print(f"rectangle :{x}")
+
+# # # circumference
+# # radius=20
+# # pi = 3.14
+# # ciecumference = 2*radius*pi
+# # print(f"circumference of circle:{ciecumference} cm")
+
+
+# # #square
+# # x_area = 12
+# # x = x_area**2
+# # print(f"area of square is:{x}cm")
+
+
+# a = int(input("enter the number"))
+# if a < 10:
+#     print("it is 1 digit")
+# if a <100:
+#     print("it is 3 digit")
+# else: a <1000
+#     print("it is 4 digit")
+
+
+# # e = int(input("enter the number:"))
+# # if e % 7 == 0:
+# #     print("yes")
+# # else:
+# #     print("No")
+
+
+# # r = int(input("enter the number:"))
+# # if r < 10:
+# #     print("oh yes")
+# # elif    r == 20:
+# #     print("oh noo")
+# # else:
+# #     print("oh yeah")
+
+# '''+ add,  - subtract,  / division it gives decimal output, // division it gives no decimal (floor division)
+# % gives reminder, ** power --- arithmetic operators'''
+# """" logical operators
+# AND logic if first value is true it will give second value means 1 and 45 = 45 
+# if first value is false it will give  first value means 0 and 23 = 0 if first value is false and second value also false it will take first value means 0 and 0 = 0
+# t and t = t
+# f and t = f
+# f and f = f
+# t and f = f"""
+# '''print(1 and 11)
+# print(0 and 0)
+# print(0 and 1)
+# print(1 and 0)'''
+# """OR logic if first value is  true it will give first value means 1 or 23 = 1
+# if first value is false it will go to second value if the second value is true it will take second value 0 or 22 = 22
+# if first value is false and second value is also false  it is false it gives second value 0 or 0 = 0
+# if first value is true and second value is false it will take first value means 1 or 0 = 1
+# t or t = t
+# t or f =t
+# f or f = f
+# f or t = t
+#  """
+
+# '''print( 0 or 0)
+# print(1 or 0)
+# print(0 or 1)
+# print(1 or 1)'''
+
+# t = int(input("enter the number"))
+# if 84%t ==0:
+#     print("done")
+# else:
+#     print("no")
+
+# division means number is divisible by some number or not (Write a program to check a number is divisible by 7 or not. t%7==0)
+# factor means the number which they already given can be divisible by that or not(Write a program to check a number is factor of 84 or not. 84%t==0)
+
+# a = int(input("enter the number"))
+# if 96%a ==0:
+#     print(f"{a} is factor of 96")
+# elif a > 96:
+#     print("invalid input")
+# else:
+#     print(f"{a} not the factor of 96")
+
+
+# Write a program to check a number is multiple of 3 or not.
+
+# a = int(input("enter the number:"))
+# if a%3 ==0:
+#     print(f"{a} is multiply by 3")
+# else:
+#     print(f"{a} not multiply by 3")
+
+# Write a program to check a number is positive or not.
+# a =int(input("enter the number:"))
+# if a > 0:
+#     print("it is positive")
+# elif a == 0:
+#     print("it is zero")
+# else:
+#     print("it is negative")
+
+# Write a program to check a number is having 4 at units place (984). when ever we divide by 10 (%10) it will be last digit
+# ((unit place means we have to %10 ))
+# (for tens place //10)%10
+#  (for hundred place //100)%10
+#  (for thousand place //1000)%10)
+# a = 984
+# unit_place = (a%10)
+# print(unit_place)
+
+# Write a program to check a number is 3 digited number or not.
+# a = int(input("enter the number"))
+# if (a >99 and a<1000):
+#     print("it is 3 digit number")
+# else:
+#     print("it is not 3 digit number")
+
+# Write a program to print even or odd for a given number
+# a  =int(input("enter the number"))
+# if a%2 == 0:
+#     print("it is even number")
+# else:
+#     print("it is odd number")
+
+# Write a program to print the greatest number in given two numbers
+# Write a program to print the least number in given two numbers
+
+# a = int(input("enter the number"))
+# b = int(input("enter the number"))
+# if a > b:
+#     print("a is greater")
+# else:
+#     if a ==b:
+#         print("both are equal")
+#     else:
+#         print("b is greater")
+
+
+# Write a program to print the given two numbers in ascending order
+# Write a program to print the given two numbers in descending order
+
+# a = int(input("enter the number:"))
+# b = int(input("enter the number:"))
+# if a >b:
+#     print(a,b)
+# else:
+#     print(b,a)
+
+# Write a program to print the greatest number in given three numbers
+
+# a = int(input("enter the first number:"))
+# b = int(input("enter the second number:"))
+# c = int(input("enter the third number:"))
+# if (a > b and c):
+#     print(f"{a} a is grater")
+# else:
+   
+#     if (b > a and b > c):
+#         print(f"{b} b is grater")
+#     else:
+#         print(f"{c} c is grater")
+       
+# Write a program to print the least number in given three numbers
+
+# a = int(input("enter the first number:"))
+# b = int(input("enter the second number:"))
+# c = int(input("enter the third number:"))
+
+# if (a < b and a < c):
+#     print(f"{a} a is smaller")
+# else:
+#     if(b< c and b <a):
+#         print(f"{b} b is smaller")
+#     else:
+#         print(f"{c} c is smaller")
+
+# Write a program to print the given three numbers in ascending order for ascending order small to big use this <
+
+# a = int(input("enter the first number:"))
+# b = int(input("enter the second number:"))
+# c = int(input("enter the third number:"))
+
+# if (a < b and a< c):
+#     if b<c:
+#         print(a,b,c)
+#     else:
+#         print(a,c,b)
+# elif(b<a and b<c):
+#     if a<c:
+#         print(b,a,c)
+#     else:
+#         print(b,c,a)
+# else:
+#     if(a<b):
+#         print(c,a,b)
+#     else:
+#         print(c,b,a)
+
+
+# Write a program to print the given three numbers in descending order for descending order big to small so use >
+
+
+# a = int(input("enter the first number:"))
+# b = int(input("enter the second number:"))
+# c = int(input("enter the third number:"))
+
+# if (a>b and a>c):
+#     if b>c:
+#         print(a,b,c)
+#     else:
+#         print(c,a,b)
+# elif (b>a and b>c):
+#     if a>c:
+#         print(b,a,c)
+#     else:
+#         print(b,c,a)
+# else:
+#     if a>b:
+#         print(c,a,b)
+#     else:
+#         print(c,b,a)
+
+# Write a program to take marks of three subjects and print the grade of student
+
+mark= int(input("enter the first mark:"))
+mark = int(input("enter the second mark:"))
+mark= int(input("enter the third mark:"))
+
+if mark <=34:
+    print("f grade")
+
+else:
+    if mark <= 70:
+        print("b grade")
+    else: 
+        print("a grade")
+        
+      
+
+
+
+
+
+
+
+
+
+
+
+
