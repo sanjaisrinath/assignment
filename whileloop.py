33c0ec133657cd12e95c0b512475f463dd296707
sanjaisrinath <134049949+sanjaisrinath@users.noreply.github.com>
2025-04-18 22:03:19 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,77 @@
+# Write a program to print 1 to 10 numbers using a while loop
+# a = 1
+# while a <=10:
+#     print(f"{a}")
+#     a = a+1
+
+# Write a program to print 10 to 1 numbers using a while loop
+# a =10
+# while a>=1:
+#     print(f'{a}')
+#     a = a-1
+
+# Write a program to print 5 to 15 numbers using a while loop
+# a = 5
+# while a<=15:
+#     print(f"{a}")
+#     a = a+1
+
+# Write a program to print 15 to 10 numbers using a while loop
+# a= 15
+# while a >=10:
+#     print(f"{a}")
+#     a =a-1
+
+# Write a program to print 1 to 10 even numbers using a while loop
+# a = 1
+# while a<=10:
+#     if  a%2==0:
+#         print(f"{a}")
+#     a = a+1
+
+# Write a program to print 1 to 10 odd numbers using a while loop
+# a = 1
+# while a<=10:
+#     if a%3==0:
+#         print(f'{a}')
+#     a =a+1
+
+# Write a program to print first ten multiples of 4 using a while loop (multiples of 4 are 4,8,12,16,20,.....)
+a = 1
+while a<=20:
+    print(a*4)
+    a =a+1
+
+# Write a program to print multiplication table of 6 using while loop
+# a = 1
+# while a<=10:
+#     print("6 x", a, "=", 6*a)
+#     a=a+1
+
+# Print the squares of all numbers from 1 to 10.
+
+# a =1
+# while a<=10:
+#     print(f"{a} is {a**2}")
+#     a =a+1
+
+# Print the cubes of all numbers from 1 to 10.
+# a =1
+# while a<=10:
+#     print(f"{a} is {a**3}")
+#     a =a+1
+
+# Write a program to print factors of 24 using while loop (factors of 24 are 1,2,3,4,6,8,12,24)
+
+# a = 24
+# b=1
+# while b <= a:
+#     if a%b==0:
+#         print(b)
+#     b=b+1
+
+
+
+
+
+
