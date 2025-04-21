ff7314b07d85cf020ced67276436e78597fea16b
sanjaisrinath <134049949+sanjaisrinath@users.noreply.github.com>
2025-04-21 23:35:56 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,68 @@
+
+# Write a program to print the number at 10000’s place
+# num = int(input("enter the number: "))
+# a = (num//10000)%10
+# print(a)
+
+#to reverse the number in while 
+# num = int(input("enter the number"))
+# sum = 0
+# while num>0:
+#     a = num%10
+#     sum = sum*10+a
+#     num = num//10
+# print(sum)
+
+# Write a program to print the sum of 1 to 10 ( initialisation of sum variable with 0 )
+# sum = 0
+# for x in range(1,11):
+#     sum =sum +x
+# print(sum)
+    
+# sum = 0
+# a = int(input('enter the starting number'))
+# b= int(input("enter the ending number"))
+# for x in range (a,b+1):
+#     sum = sum +x
+# print(sum)
+
+# Write a program to print the average of 1 to 10 --- to find average we have to use (end - start point +1) 10 - 1 +1 = 10
+# avr = 0
+# # a = int(input('enter the starting number'))
+# for x in range (1,11):
+#     avr = avr+x
+# print(avr/10)
+
+# Write a program to print the sum of the numbers between the given two numbers
+# sum = 0
+# a = int(input("enter the number a "))
+# b = int(input("enter the number b "))
+# for x in range (a,b+1):
+#     sum = sum + x
+# print(sum)
+
+# Write a program to print the sum of even numbers between the given two numbers
+# sum = 0
+# a = int(input("enter the number a "))
+# b = int(input("enter the number b "))
+# for x in range (a,b+1):
+#     if x%2==0:
+#         sum = sum +x
+# print(sum)
+
+# Write a program to print the sum of odd numbers between the given two numbers
+# sum = 0
+# a = int(input("enter the number a "))
+# b = int(input("enter the number b "))
+# for x in range (a,b+1):
+#     if x%3==0:
+#         sum = sum +x
+# print(sum)
+
+# Write a program to print factorial of a given number (for factorial we have to use 1 because when we use 1 we can get factorial when we use 0 we get 0 because 0*1 is 0 )
+# factorial = 1
+# a = int(input("enter the number"))
+# for x in range (1,a+1):
+#     factorial = factorial*x
+# print(factorial)
+
