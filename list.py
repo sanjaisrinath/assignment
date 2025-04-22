b06b5e651e94341cd5b513010c374baef3d2f04b
sanjaisrinath <134049949+sanjaisrinath@users.noreply.github.com>
2025-04-22 22:56:37 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,5 @@
+'''data types - int (number), string(str), boolean(bool) -- stores single value
+aprat from this we have some other datatypes
+sequence / mapping datatypes -- which stores more than one value at a time this both is knows as iterables
+sewuence data type -- list, str, tuples (str will store only one value remainig will store more then one value )
+mapping data types -- dict --''' 
