5c0fd01aaf65a5da4a9e6b091ee53fe93be61d3b
sanjaisrinath <134049949+sanjaisrinath@users.noreply.github.com>
2025-04-26 02:07:44 +0530

    list.py

--- list.py
+++ list.py
@@ -2,4 +2,9 @@
 aprat from this we have some other datatypes
 sequence / mapping datatypes -- which stores more than one value at a time this both is knows as iterables
 sewuence data type -- list, str, tuples (str will store only one value remainig will store more then one value )
-mapping data types -- dict --''' 
+mapping data types -- dict --
+list is of 2 types homogenous and heterogenous list
+homogenous list contains only single data type ie: str or int [1,1,2,2] ["srr","eeje"]
+heterogenous list contains all data types ie: str,int,boolean [1,"sss",22,33,33,"se",true]
+list are muteable means we can change the list but in str we cannot changed str is immuteable 
+when we add two arrays into one array this process is called concadination''' 
